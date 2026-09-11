fe9fce9f5ace4e47ebff13e9cdbe7f5cfd249979
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-11 20:16:34 -0300

    Aula-8

--- app/Policies/ProdutoPolicy.php
+++ app/Policies/ProdutoPolicy.php
@@ -2,65 +2,38 @@
 
 namespace App\Policies;
 
+use App\Enums\UserRole;
 use App\Models\Produto;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class ProdutoPolicy
 {
     /**
-     * Determine whether the user can view any models.
+     * Create a new policy instance.
      */
-    public function viewAny(User $user): bool
+
+    public function view(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Produto $produto): bool
+    public function viewAll(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
     public function create(User $user): bool
     {
-        return false;
+        return $user->role === UserRole::ADMIN;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Produto $produto): bool
+    public function edit(User $user, Produto $produto): bool
     {
-        return false;
+        return $user->role === UserRole::ADMIN && $produto->user_id === $user->id;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
     public function delete(User $user, Produto $produto): bool
     {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Produto $produto): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Produto $produto): bool
-    {
-        return false;
+        return $user->role === UserRole::ADMIN && $produto->user_id === $user->id;
     }
 }
