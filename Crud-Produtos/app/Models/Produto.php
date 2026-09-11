9ed252fd7dc9a4ea8317cc87a3d0dd629b868f2c
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-11 19:33:38 -0300

    Aula-3

--- Crud-Produtos/app/Models/Produto.php
+++ Crud-Produtos/app/Models/Produto.php
@@ -3,13 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Produto extends Model
 {
-   
+    /* Atributos que podem ser preenchidos por
+    mass assignment*/
     protected $fillable = [
         'nome', 
         'preco', 
         'quantidade',
+        'categoria_id',
         ];
+    
+        public function categoria(): BelongsTo
+        {
+            return $this->belongsTo(Categoria::class);
+        }
 }
